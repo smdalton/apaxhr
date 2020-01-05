06b15ab59af63e1e96a6b76d72dd3f4b872dcba0
smd <smdalton@ucsc.edu>
2020-01-05 20:11:13 +0700

    completed custom user

--- app/core_hr/admin.py
+++ app/core_hr/admin.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.apps import apps
-from . import models
-from .admin_data import inlines
+# from django.contrib import admin
+# from django.apps import apps
+# from . import models
+# from .admin_data import inlines
 # Register your hr_models here.
 
 #
@@ -14,18 +14,18 @@ from .admin_data import inlines
 # class PassportAdmin(admin.ModelAdmin):
 #     autocomplete_fields = ('owner',)
 
-
-class EmployeeAdmin(admin.ModelAdmin):
-    model = models.Employee
-    fields = ['middle_name','bio','employee_role']
-    search_fields = ['user__first_name','user__last_name','middle_name']
-    inlines= [
-        inlines.PassportInline,
-        #inlines.WorkPermitInline,
-        #inlines.RegistryOfStayInline
-    ]
-
-admin.site.register(models.Employee, EmployeeAdmin)
+#
+# class EmployeeAdmin(admin.ModelAdmin):
+#     model = models.Employee
+#     fields = ['middle_name','bio','employee_role']
+#     search_fields = ['user__first_name','user__last_name','middle_name']
+#     inlines= [
+#         inlines.PassportInline,
+#         #inlines.WorkPermitInline,
+#         #inlines.RegistryOfStayInline
+#     ]
+#
+# admin.site.register(models.Employee, EmployeeAdmin)
 
     #
 
