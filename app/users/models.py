21505669389bd069e7a25b8bc5c3389fef9a1f6b
smd <smdalton@ucsc.edu>
2020-01-07 11:45:29 +0700

    added schedules

--- app/users/models.py
+++ app/users/models.py
@@ -6,15 +6,15 @@ from .managers import CustomUserManager
 
 
 class Employee(AbstractBaseUser, PermissionsMixin):
-    # first_name = models.CharField(_('first name'), max_length=25, blank=False, null=True)
-    # middle_name = models.CharField(_('Middle Name'), max_length=25, blank=False, null=True)
-    # last_name = models.CharField(_('Last Name'), max_length=25, blank=False, null=True)
-    # bio = models.TextField(_('Personal Biography'), max_length=500, blank=True, null=True)
+    first_name = models.CharField(_('first name'), max_length=25, blank=False, null=True)
+    middle_name = models.CharField(_('Middle Name'), max_length=25, blank=False, null=True)
+    last_name = models.CharField(_('Last Name'), max_length=25, blank=False, null=True)
+    bio = models.TextField(_('Personal Biography'), max_length=500, blank=True, null=True)
 
     email = models.EmailField(_('email address'), unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-    # date_joined = models.DateTimeField(default=timezone.now)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
