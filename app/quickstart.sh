c7fb2a7371a22831d7fc3cb2ee0bbfd5c141990c
smd <shanemdalton@gmail.com>
2020-11-27 20:36:07 -0800

    ready for demo deployment

--- app/quickstart.sh
+++ app/quickstart.sh
@@ -5,4 +5,4 @@ python3 manage.py create_users_and_documents
 python3 manage.py create_positions
 python3 manage.py create_centers
 python3 manage.py create_permissions
-python3 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8014
